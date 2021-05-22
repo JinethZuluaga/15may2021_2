c110690e9d3984952474c26247ec1bf734302972
JinethZuluaga <jzp@misena.edu.co>
2021-05-22 06:51:09 -0500

    Consultando datos de la tabla usuarios

---
+++ 15may2021_2/Controllers/usuariosController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using _15may2021_2.Models;
+
+
+namespace _15may2021_2.Controllers
+{
+    public class usuariosController : Controller
+    {
+        // GET: usuarios
+        public ActionResult Index()
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return View(db.usuario.ToList());
+            }
+        }
+    }
+}
