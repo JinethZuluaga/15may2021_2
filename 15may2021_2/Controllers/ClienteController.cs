cd2d4bed045e81ea565cfca5bcd6146dcd02d0c1
JinethZuluaga <jzp@misena.edu.co>
2021-05-24 20:31:41 -0500

    Creacion Formulario clientes

---
+++ 15may2021_2/Controllers/ClienteController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using _15may2021_2.Models;
+
+namespace _15may2021_2.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            using (var db =  new inventario2021Entities ())
+            {
+                return View(db.cliente.ToList());
+            }                
+        }
+
+        public ActionResult Create ()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create(cliente cliente)
+        {
+            if (!ModelState.IsValid)
+                return View();
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    db.cliente.Add(cliente);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+            } catch (Exception ex)
+            {
+                ModelState.AddModelError("", "erro " + ex);
+                return View();
+            }
+        }
+    }
+}
